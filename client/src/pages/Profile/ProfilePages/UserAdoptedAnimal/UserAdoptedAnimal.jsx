79341e3c4dccf8b8cdbc4b9967e909b86a2833b0
Alexey <AlexeyKom2703@yandex.ru>
2025-02-05 13:23:18 +0300

    Внесение изменений в проект

--- client/src/pages/Profile/ProfilePages/UserAdoptedAnimal/UserAdoptedAnimal.jsx
+++ client/src/pages/Profile/ProfilePages/UserAdoptedAnimal/UserAdoptedAnimal.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+//===== Ресурсы =====//
+import './UserAdoptedAnimal.scss';
+
 const UserAdoptedAnimal = () => {
   return (
-    <div>
-      UserAdoptedAnimal
+    <div className='UserAdoptedAnimal'>
+      <h2 className="UserAdoptedAnimal__title">Ваши усыновленные животные</h2>
+      <div className="UserAdoptedAnimal__content">
+
+        <h4>Список усыновленных животных</h4>
+
+      </div>
     </div>
   )
 }
