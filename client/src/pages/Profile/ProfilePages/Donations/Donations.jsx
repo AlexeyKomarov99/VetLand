f14dd54472061952a61f38ddfc7cd1f55d097d95
Alexey <AleksKom2703@yandex.ru>
2025-02-10 18:08:03 +0300

    dd

--- client/src/pages/Profile/ProfilePages/Donations/Donations.jsx
+++ client/src/pages/Profile/ProfilePages/Donations/Donations.jsx
@@ -3,13 +3,37 @@ import React from 'react';
 //===== Ресурсы =====//
 import './Donations.scss';
 
+//===== Компоненты =====//
+import DonationsShelter from '../../../../components/Profile/Common/DonationsShelter/DonationsShelter';
+import DonationsAnimals from '../../../../components/Profile/Common/DonationsAnimals/DonationsAnimals';
+import UserRatingDonations from '../../../../components/Profile/Common/UserRatingDonations/UserRatingDonations';
+
+//===== API =====//
+
 const Donations = () => {
   return (
     <div className='Donations'>
       <h2 className="Donations__title">Пожертвование</h2>
       <div className="Donations__content">
 
-        <h4>Пожертвовать животному</h4>
+        {/* Группа таблиц с пожертвованиями */}
+        <div className="Donations__tables-group">
+          
+          {/* Таблица "Пожертвования приюту" */}
+          <DonationsShelter />
+
+          {/* Таблица "Пожертвования животным" */}
+          <DonationsAnimals />
+
+        </div>
+
+        {/* Дать разрешение на отображение своих ПД в общей таблице пожертвований? */}
+          <div className="Donations__give-consent">
+            {/* label */}
+          </div>
+
+        {/* Таблица "Общий список пожертвований всех пользователей" */}
+        <UserRatingDonations />
 
       </div>
     </div>
