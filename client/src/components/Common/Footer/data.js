6e9e0df2d2263ba2e1577fc104d5119fe7c5b368
Alexey <AleksKom2703@yandex.ru>
2025-09-21 03:02:26 +0800

    Фрагментация компонента Footer на маленькие компоненты. Все svg картинки были перенесены в отдельные файлы .jsx для чистоты компонента Footer. Кроме того, все ссылки и маршруты к ним были перенесены в файл data.js также для чистоты компонента Footer

---
+++ client/src/components/Common/Footer/data.js
@@ -0,0 +1,40 @@
+//===== Fund Partners svg =====//
+import AmoCRM from "../../BrandLabel/AmoCRM"
+import ChildrensWorld from "../../BrandLabel/ChildrensWorld"
+import OzonCare from "../../BrandLabel/OzonCare"
+import RibbonGoodness from "../../BrandLabel/RibbonGoodness"
+import Sir from "../../BrandLabel/Sir"
+import VetUnion from "../../BrandLabel/VetUnion"
+import ZavrPetStore from "../../BrandLabel/ZavrPetStore"
+
+export const ourWards = [
+    {id: 1, link: '/animals', title: 'готовы ехать домой'},
+    {id: 2, link: '/animals', title: 'ждут хозяина в москв'},
+    {id: 3, link: '/animals', title: 'на лечении'},
+    {id: 4, link: '/animals', title: 'собаки'},
+    {id: 5, link: '/animals', title: 'кошки'},
+    {id: 6, link: '/animals', title: 'найти друга'},
+    {id: 7, link: '/how-take-home', title: 'как забрать домой?'}
+]
+
+export const aboutUs = [
+    {id: 1, link: '/about-us', title: 'команда'},
+    {id: 2, link: '/about-us', title: 'наша миссия'},
+    {id: 3, link: '/about-us', title: 'докуенты'},
+    {id: 4, link: '/about-us', title: 'база знаний'}
+]
+
+export const socialNetworks = [
+    {id: 1, link: 'https://vk.com/vetland_adoption', title: 'вконтакте'},
+    {id: 2, link: 'https://vk.com/vetland_adoption', title: 'telegram'},
+]
+
+export const fundPartners = [
+    {id: 1, icon: <AmoCRM />},
+    {id: 2, icon: <ChildrensWorld />},
+    {id: 3, icon: <OzonCare />},
+    {id: 4, icon: <VetUnion />},
+    {id: 5, icon: <RibbonGoodness />},
+    {id: 6, icon: <Sir />},
+    {id: 7, icon: <ZavrPetStore />}
+]
