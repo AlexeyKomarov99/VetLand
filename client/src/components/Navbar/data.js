abde9d84445e0b226f54da5e1a89d8e8d1db3140
Alexey <AleksKom2703@yandex.ru>
2025-09-21 03:44:51 +0800

    Внесение маленьких правок

--- client/src/components/Navbar/data.js
+++ client/src/components/Navbar/data.js
@@ -2,6 +2,5 @@ export const links = [
     {id: 1, title: 'Забрать домой', route: '/questionnaire'},
     {id: 2, title: 'Наши подопечные', route: '/animals'},
     {id: 3, title: 'О нас', route: '/about-us'},
-    {id: 4, title: 'Уже дома', route: '/happy-stories'},
-    {id: 5, title: 'Профиль', route: '/profile'},
+    {id: 4, title: 'Уже дома', route: '/happy-stories'}
 ]
